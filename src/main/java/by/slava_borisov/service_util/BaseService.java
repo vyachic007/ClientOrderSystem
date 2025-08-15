cda2b14cb1805d65814f2dacb1d7a8fa5a4ba388
Vyacheslav Borisov <vyachic.007@gmail.com>
2025-08-15 20:32:08 +0300

    add lombok an.

--- src/main/java/by/slava_borisov/service_util/BaseService.java
+++ src/main/java/by/slava_borisov/service_util/BaseService.java
@@ -1,15 +1,13 @@
 package by.slava_borisov.service_util;
 
 import by.slava_borisov.helper.TransactionHelper;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.SessionFactory;
 
+@RequiredArgsConstructor
 public abstract class BaseService {
 
     protected final SessionFactory sessionFactory;
     protected final TransactionHelper transactionHelper;
 
-    protected BaseService(SessionFactory sessionFactory, TransactionHelper transactionHelper) {
-        this.sessionFactory = sessionFactory;
-        this.transactionHelper = transactionHelper;
-    }
 }
