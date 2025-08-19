7c0f408a27243be443af4a7b3f75df722e380a0d
Vyacheslav Borisov <vyachic.007@gmail.com>
2025-08-19 23:57:50 +0300

    add .properties file

--- src/main/java/by/slava_borisov/config/HibernateConfiguration.java
+++ src/main/java/by/slava_borisov/config/HibernateConfiguration.java
@@ -5,25 +5,35 @@ import by.slava_borisov.entity.Coupon;
 import by.slava_borisov.entity.Order;
 import by.slava_borisov.entity.Profile;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class HibernateConfiguration {
 
+    @Autowired
+    private Environment env;
+
     @Bean
-    public static SessionFactory getSessionFactory() {
+    public SessionFactory getSessionFactory() {
         org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
 
-        configuration.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
-        configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/postgres");
-        configuration.setProperty("hibernate.connection.username", "postgres");
-        configuration.setProperty("hibernate.connection.password", "root");
+        configuration.setProperty("hibernate.connection.driver_class", env.getProperty("db.driver"));
+        configuration.setProperty("hibernate.connection.url", env.getProperty("db.url"));
+        configuration.setProperty("hibernate.connection.username", env.getProperty("db.username"));
+        configuration.setProperty("hibernate.connection.password", env.getProperty("db.password"));
 
-        configuration.setProperty("hibernate.show_sql", "true");
-        configuration.setProperty("hibernate.format_sql", "true");
-        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+        configuration.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql",
+                "true"));
+        configuration.setProperty("hibernate.format_sql", env.getProperty("hibernate.format_sql",
+                "true"));
+        configuration.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        configuration.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto",
+                "update"));
 
         configuration.addAnnotatedClass(Client.class);
         configuration.addAnnotatedClass(Profile.class);
